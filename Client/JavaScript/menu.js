06bc97e05bb4c0ebb83a79f431ea16d3e9efc85e
Timo <T.Jugariu@student.tudelft.nl>
2017-07-28 18:38:22 +0200

    Neat shift between screens added

--- Client/JavaScript/menu.js
+++ Client/JavaScript/menu.js
@@ -1,4 +1,4 @@
-window.adaptContent = function() {
+function adaptContent() {
 	var bar = document.querySelector("div.bar:nth-of-type(1)").getBoundingClientRect();
 	var t = bar.height;
 	var h = window.innerHeight - bar.height;
@@ -11,7 +11,27 @@ function main() {
 		window.open("/HTML/waiting.html?gm="+gameMode);
 	})
 
+	$("div.bar").on("click", function() {
+		if($(this).is(":nth-of-type(1)")) {
+			$("div#container").css("transform","translate(0%, 0%)");
+		}
+		else if($(this).is(":nth-of-type(2)")) {
+			$("div#container").css("transform","translate(-50%, 0%)");
+		}
+		else if($(this).is(":nth-of-type(3)")) {
+			$("div#container").css("transform","translate(0%, -50%)");
+		}
+		else {
+			$("div#container").css("transform","translate(-50%, -50%)");
+		}
+	})
+
+	$(window).on("resize", function() {
+		adaptContent();
+	})
+
 	adaptContent();
+
 }
 
-$(document).ready(main);
+$(window).on("load", main);
